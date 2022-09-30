d5749f58a125d26d32478a7022c7c53d7adfd5b2
Juan De La Cruz <jedelacruzn@gmail.com>
2022-09-30 12:28:33 -0500

    refactor: implements missing specs for storybook

--- components/cards/car-detail-card/CarDetailCard.mocks.ts
+++ components/cards/car-detail-card/CarDetailCard.mocks.ts
@@ -2,24 +2,7 @@ import { ICarDetailCard } from './CarDetailCard';
 
 const base: ICarDetailCard = {
   headerText: 'EXTERIOR',
-  specs: [
-    {
-      name: 'Cylinders',
-      value: 'L4',
-    },
-    {
-      name: 'City MPG',
-      value: '20 MPG',
-    },
-    {
-      name: 'Highway MPG',
-      value: '25 MPG',
-    },
-    {
-      name: 'Engine',
-      value: '1.3',
-    },
-  ],
+  specs: { cylinders: 'L4', cityMPG: 23, highwayMPG: 25, engine: 1.3 },
 };
 
 export const mockCarDetailCardProps = {
